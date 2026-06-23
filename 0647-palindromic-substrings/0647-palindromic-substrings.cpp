94cd6652ec7e835cec2a5541f2fa260719867faa
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-06-23 21:54:17 +0530

    Time: 4 ms (68.69%), Space: 8.5 MB (77.91%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,23 @@
 class Solution {
-public: 
-       bool isPal(string s){
-           int left = 0;
-           int right = s.size()-1;
-           while(left < right){
-            if(s[left] != s[right]) return false;
-            left++;
-            right--;
-           }
-           return true;
-       }
-        
+public:     
     int countSubstrings(string s) {
         int count = 0;
         int n = s.size();
          for(int i = 0 ;i < n;i++ ){
-            for(int j = i ; j < n ; j++){
-                if(isPal(s.substr(i,(j-i)+1))) count++;
-              
-            }
+             int left = i;
+             int right = i;
+             while(left >= 0 && right < n && s[left] == s[right]){
+                count++;
+                left--;
+                right++;
+             }
+             left = i;
+             right = i+1;
+             while(left >= 0 && right < n && s[left] == s[right]){
+                count++;
+                left--;
+                right++;
+             }
          }
          return count;
     }
