0e5532b148ef00d649044c0b27a1273493aff3e6
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-07-26 19:42:55 +0530

    Time: 0 ms (100%), Space: 31.6 MB (10.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        priority_queue<int,vector<int> , greater<int>>pq;
-        priority_queue<int,vector<int>>max_heap;
-        for(int num : nums){
-            pq.push(num);
-            max_heap.push(num);
-            if(pq.size() > 3) pq.pop();
-            if(max_heap.size() > 2) max_heap.pop();
-        }
+          int min1 = INT_MAX;
+          int min2 = INT_MAX;
+          int mx1 = INT_MIN;
+           int mx2 = INT_MIN;
+            int mx3 = INT_MIN;
 
-        int p = 1;
-        int n = 1;
-         n =  1 ;
-          while(!max_heap.empty()) {
-             n*=max_heap.top();
-             max_heap.pop();
-          } 
+            for(int x : nums){
+                 if(x <= min1){
+                    min2 = min1;
+                    min1 = x;
+                 }
+                 else min2 = min(min2,x);
+            }
+              for(int x : nums){
+                 if(x >= mx1){
+                    mx3 = mx2;
+                    mx2 = mx1;
+                    mx1 = x;
+                 }
+                 else if( x >= mx2 ){
+                     mx3 = mx2;
+                    mx2 = x ;
+                 }
+                 else mx3 = max(mx3,x);
+            }
 
-        while(!pq.empty()){
-             p*=pq.top();
-             if(pq.size() == 1) n*=pq.top();
-             pq.pop();
-             
-        }
-        return max(p,n);
+            long long p = mx1*mx2;
+            p*=mx3;
+           long long  n = min1*min2;
+           n*=mx1;
+        return int(max(p,n));
     }
 };
