d280cf1c849ff20da77d1ec8f627195b00344b23
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-07-31 14:43:12 +0530

    Time: 2 ms (49.19%), Space: 15.7 MB (77.38%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,29 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>>nodes;
-        queue<pair<TreeNode* , pair<int,int>>>q;
+       map<int,map<int,multiset<int>>>mpp;
+       if(!root) return{{}};
+       queue<pair<TreeNode*,pair<int,int>>>q;
         q.push({root,{0,0}});
         while(!q.empty()){
-             auto p = q.front();
-             q.pop();
-             TreeNode* node = p.first;
-             int x = p.second.first;
-             int y = p.second.second ;
-             nodes[x][y].insert(node->val);
-
-             if(node->left) q.push({node->left,{x-1,y+1}});
-             if(node->right) q.push({node->right,{x+1,y+1}});
-
+            int k =q.size();
+            while(k--){
+                auto it = q.front();
+                auto node = it.first;
+                int col = it.second.second;
+                int row = it.second.first;
+                q.pop();
+                mpp[col][row].insert(node->val);
+                if(node->left) q.push({node->left,{row+1,col-1}});
+                if(node->right) q.push({node->right,{row+1,col+1}}); 
+            } 
         }
         vector<vector<int>>ans;
-        for(auto &col : nodes){
-           vector<int>temp;
-           for(auto &row : col.second ){
-             temp.insert(temp.end(), row.second.begin(), row.second.end());
-           }
-           ans.push_back(temp);
+        for(auto &col : mpp){
+             vector<int>a;
+             for(auto &rows : col.second ){
+                 a.insert(a.end(),rows.second.begin(),rows.second.end());
+             }
+           ans.push_back(a);
+
         }
         return ans;
+        
     }
 };
