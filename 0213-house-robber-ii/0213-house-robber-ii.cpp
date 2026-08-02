0be79c63c3c0d050ea2a47e0038b63da980925c7
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-02 15:10:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (37.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
-
-     int helper(vector<int>&nums,int start,int end,vector<vector<int>>&dp){
-         if(start > end) return 0;
-         if(dp[start][end] != -1) return dp[start][end];
-          int take = nums[end]+helper(nums,start,end-2,dp);
-          int leave = helper(nums,start,end-1,dp);
-          return dp[start][end] =  max(leave,take);
-     }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       int first = helper(nums,0,n-2,dp);
-   vector<vector<int>>dp2(n+1,vector<int>(n+1,-1));
-    int last = helper(nums,1,n-1,dp2);
-       return max(first,last);
+        if(n <= 1) return nums[0];
+         vector<int>dp1(n,0);
+         for(int i = 0;i<n-1;i++){
+             int prev1 = 0;
+              int prev2 = 0; 
+            if(i > 0) prev1 = dp1[i-1];
+            if(i > 1) prev2 = dp1[i-2];
+              dp1[i] = max(nums[i]+prev2 , prev1);
+         }
+         vector<int>dp2(n,0);
+         for(int i = 1;i<n;i++){
+             int prev1 = 0;
+              int prev2 = 0; 
+            if(i > 1) prev1 = dp2[i-1];
+            if(i > 2) prev2 = dp2[i-2];
+              dp2[i] = max(nums[i]+prev2 , prev1);
+         }
+         return max(dp1[n-2],dp2[n-1]);
+
     }
 };
