028c8d5bbdb2f8b16c2bf34bbf0bf191db800915
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-03 23:38:34 +0530

    Time: 3 ms (38.72%), Space: 15 MB (47.16%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n =  grid.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>prev(n,0);
         for(int i = 0;i<n;i++){
-            dp[0][i] = grid[0][i];
+            prev[i] = grid[0][i];
         }
         for(int i = 1;i<n;i++){
+            vector<int>cur(n,0);
             for(int j = 0;j<n;j++){
               int left = INT_MAX,center = INT_MAX,right = INT_MAX;
-              if(j > 0) left = dp[i-1][j-1];
-              if(j+1 < n) right = dp[i-1][j+1];
-              center = dp[i-1][j];
-              dp[i][j] = grid[i][j] + min(center,min(left,right));
+              if(j > 0) left = prev[j-1];
+              if(j+1 < n) right = prev[j+1];
+              center = prev[j];
+              cur[j] = grid[i][j] + min(center,min(left,right));
             }
+            prev = cur;
         }
         
         int minPath = INT_MAX;
           for(int i = 0;i<n;i++){
-            minPath = min(dp[n-1][i],minPath);
+            minPath = min(prev[i],minPath);
         }
         return minPath;
          
