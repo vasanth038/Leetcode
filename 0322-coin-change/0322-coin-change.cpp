2e496468cadb78f722179208d4e6e5e43cfa9efe
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-10 01:03:57 +0530

    Time: 39 ms (48.85%), Space: 48.8 MB (42.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,25 @@
 class Solution {
-  int   helper(vector<int>&coins ,int i,int amount, vector<vector<int>>&dp){
-         if(i < 0 ) return 1e9;
-         if(amount == 0) return 0;
-         if(dp[i][amount] != -1) return dp[i][amount];
-         int take = 1e9;
-          if(coins[i] <= amount)  take = 1+ helper(coins , i , amount-coins[i],dp) ;
-          int nottake = helper(coins,i-1,amount,dp); 
-
-          return dp[i][amount] = min(take,nottake);
-     }
 public:
     int coinChange(vector<int>& coins, int amount) {
      int n = coins.size();
-     vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-      int ans = helper(coins,n-1,amount,dp);
-      if(ans >= 1e9) return -1;
-      
-      return ans;
+     vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
+     for(int i = 0; i<n;i++) dp[i][0] = 0;
+       for(int k = 0;k<=amount;k++){
+        if(coins[0] <= k) dp[0][k] = 1+ dp[0][k-coins[0]];
+       }
+      for(int i = 1;i<n;i++){
+         for(int k = 0 ;k <= amount;k++){
+             int take = 1e9;
+             int nottake = dp[i-1][k];
+             if(coins[i] <= k) take = 1+dp[i][k-coins[i]];
+             dp[i][k] = min(take,nottake);
+         }
+      }
+
+      if(dp[n-1][amount] < 1e9) return dp[n-1][amount];
+      return -1;
+
+        
     
     }
 };
