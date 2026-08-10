c394ae4d0d9cff0479c244880278b0f38743bcf7
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-10 21:40:27 +0530

    Time: 7 ms (65.92%), Space: 15.1 MB (58.9%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -5,18 +5,20 @@ public:
     int sum = accumulate(nums.begin(),nums.end(),0);
     if( sum-target < 0 || (sum-target)%2 != 0) return 0;
     int t = (sum-target)/2;
-     vector<vector<int>>dp(n,vector<int>(t+1,0));
-     dp[0][0] = 1;
-     if(nums[0] <= t ) dp[0][nums[0]]++;
+      vector<int>prev(t+1,0);
+     prev[0] = 1;
+     if(nums[0] <= t ) prev[nums[0]]++;
         for(int i =1;i<n;i++){
+            vector<int>cur(t+1,0);
              for(int k = 0;k<=t;k++){
-                int nottake = dp[i-1][k];
+                int nottake = prev[k];
                 int take = 0;
-                if(nums[i] <= k) take = dp[i-1][k-nums[i]];
-                dp[i][k] = take+nottake;
+                if(nums[i] <= k) take = prev[k-nums[i]];
+                cur[k] = take+nottake;
              }
+          prev = cur;
         }
 
-        return dp[n-1][t];
+        return prev[t];
 }    
 };
