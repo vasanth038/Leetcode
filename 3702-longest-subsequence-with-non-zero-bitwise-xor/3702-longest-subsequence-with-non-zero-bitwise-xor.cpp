bcf70717c127e235966fbb49415d1c6f4d706c5a
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-15 09:12:34 +0530

    Time: 168 ms (5.26%), Space: 173.5 MB (1.97%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -4,34 +4,14 @@ public:
         
         sort(nums.rbegin(),nums.rend());
        if(nums[0] == 0) return 0;
-        vector<long long >bits(31,0);
-        for(int n : nums){
-             int p = 0;
-             while(n > 0){
-                 if(n&1){
-                     bits[p]++;
-                 }
-                 p++;
-                 n = n >> 1;
-             }
+        
+        int X = 0;
+        for(int num : nums){
+             X^=num;
         }
+        int n =nums.size();
+        if(X) return n;
 
-        int n = nums.size();
-        long long odd = 0;
-        long long even = 0;
-        for(int i = 30 ; i>=0;i--){
-             if(bits[i] == 0) {
-                continue;
-             }
-             if(bits[i]&1){
-                 odd++;
-                 break;
-             }
-             else {
-                even++;
-             }
-        }
-        if(odd) return n;
-      return n - 1;
+        return n-1;
     }
 };
