75a170370124b5d1a4ac8aa3ebb6d492cb12816d
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-20 19:01:34 +0530

    Time: 89 ms (64.23%), Space: 87.8 MB (64.4%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -7,19 +7,14 @@ public:
 
         for(int i = n-1;i>=0;i--){
              vector<int>cur(2,0);
-             for(int buy = 0 ;buy<=1;buy++){
-                 
-                 if(buy){
                      int buyNow = -prices[i]-fee+prev[0];
                      int buyNext = prev[1];
-                     cur[buy] = max(buyNext,buyNow);
-                 }
-                 else{
                     int sellNow = prices[i]+prev[1];
                     int sellNext = prev[0];
-                    cur[buy] = max(sellNow , sellNext);
-                 }
-             }
+
+                    
+                     cur[1] = max(buyNext,buyNow);
+                    cur[0] = max(sellNow , sellNext);
              swap(cur,prev);
         }
 
