a7e1bad719f050a76ff2f511cd52fbe2a27b4d40
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-23 17:32:42 +0530

    Time: 23 ms (65.86%), Space: 14 MB (54.85%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
             int m = cuts.size();
-        cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
-        sort(cuts.begin(),cuts.end());
+           cuts.push_back(n);
+           cuts.push_back(0);
+           sort(cuts.begin(),cuts.end());
 
-         vector<vector<int>>dp(m+2,vector<int>(m+2,0));
-        for(int i = m;i >=1;i--){
-            for(int j = 1;j <= m;j++){
-               if(i > j) continue;
+           vector<vector<int>>dp(m+2,vector<int>(m+2));
+
+           for(int i = m ; i>=1;i--){
+
+            for(int j = 1;j<=m;j++){
+                if(i > j) continue;
+
                 int cost = 1e9;
-                for(int k = i ; k <= j;k++){
-                    cost = min(cost,(cuts[j+1] - cuts[i-1])+dp[i][k-1]+dp[k+1][j]);
+
+                for(int k = i ; k<=j;k++){
+                    int cur = (cuts[j+1] - cuts[i-1])+dp[i][k-1]+dp[k+1][j];
+                    cost = min(cost ,cur);
                 }
-
-                dp[i][j] = cost;
+               dp[i][j] = cost;
             }
-        }
+           }
 
-        return dp[1][m];
-
+           return dp[1][m];
     }
 };
