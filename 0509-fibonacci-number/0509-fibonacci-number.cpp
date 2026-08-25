efe3a933e7de7adefb4b4da62fb5e8bdfea49861
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-25 23:59:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,13 +3,16 @@ public:
     
     int fib(int n) {
       
-      vector<int>dp(n+2,0);
-       dp[0] = 0;
-       dp[1] = 1;
+      if(n <= 1) return n;
+     
+       int prev2 = 0;
+       int prev1 = 1;
       for(int i = 2;i<=n;i++){
-        dp[i] = dp[i-1]+dp[i-2];
+        int cur = prev1+prev2;
+         prev2 = prev1;
+         prev1 = cur;
       }
 
-      return dp[n];
+      return prev1;
     }
 };
