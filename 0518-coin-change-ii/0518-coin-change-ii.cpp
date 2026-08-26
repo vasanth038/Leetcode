24d3ae6fbe1f0f25a342fa7c814ca7c047cc5abd
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-26 18:21:56 +0530

    Time: 25 ms (70.84%), Space: 35.6 MB (69.43%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
 
     int change(int amount, vector<int>& coins) {
-       
-       int n = coins.size();
 
-       vector<int>prev(amount+1,0);
-       prev[0] = 1;
-       for(int k = 0;k <= amount ;k++){
-        if(coins[0] <= k) prev[k] = prev[k-coins[0]];
-       }
-  
-       for(int i =1 ;i<n;i++){
-         vector<int>cur(amount+1,0);
-        for(int k = 0 ;k<=amount;k++){
-             int nottake = prev[k];
-             int take = 0;
-             if(coins[i] <= k) take = cur[k-coins[i]];
-             cur[k] = take+nottake; 
+
+        int n = coins.size();
+
+        vector<unsigned int>prev(amount+1,0);
+
+        prev[0] = 1;
+         
+        for(int k = 1; k<= amount;k++){
+            if(coins[0] <= k ) prev[k] = prev[k-coins[0]];
         }
-        prev = cur;
-       }
 
-       return prev[amount];
+
+        for(int i = 1;i<n;i++){
+               vector<unsigned int>cur(amount+1,0);
+               cur[0] = 1;
+            for(int k = 1;k<=amount;k++){
+                if(coins[i] <= k) cur[k] = prev[k]+cur[k-coins[i]];
+                else cur[k] = prev[k];
+            }
+            swap(cur,prev);
+        }
+
+        return prev[amount];
  
     }
 };
