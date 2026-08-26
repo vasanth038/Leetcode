1e892114f1f8b65bb1e06a6f7f705a7c4f4c9789
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-26 12:04:13 +0530

    Time: 3 ms (85.99%), Space: 15.4 MB (55.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-     int n = nums.size();
+    
     int sum = accumulate(nums.begin(),nums.end(),0);
-    if( sum-target < 0 || (sum-target)%2 != 0) return 0;
-    int t = (sum-target)/2;
-      vector<int>prev(t+1,0);
-     prev[0] = 1;
-     if(nums[0] <= t ) prev[nums[0]]++;
-        for(int i =1;i<n;i++){
-            vector<int>cur(t+1,0);
-             for(int k = 0;k<=t;k++){
-                int nottake = prev[k];
-                int take = 0;
-                if(nums[i] <= k) take = prev[k-nums[i]];
-                cur[k] = take+nottake;
-             }
-          prev = cur;
-        }
+    if(sum-target < 0 || (sum-target)%2 == 1) return 0;
+    int n = nums.size();
+    // s1+s2 == sum , s1-s2 == target , (sum-target)%2 == 0 && >=0 
+    // when last one element is left if(target == 0) ||  nums[0] == target return 1;
+    // 2 choice take or nottake : helper(i-1,target-nums[i]) ,helper(i-1,target);
+      
+       int s =  (sum-target)/2 ;
+    vector<vector<int>>dp(n,vector<int>(s+1,0));
 
-        return prev[t];
+    dp[0][0] = 1;
+    if(nums[0] <= s) dp[0][nums[0]] += 1;
+
+    for(int i = 1;i<n;i++){
+
+        for(int j = 0;j<=s;j++){
+            if(nums[i] <= j) dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i]];
+            else dp[i][j] = dp[i-1][j];
+        }
+    }
+
+
+
+return dp[n-1][s];
+    
 }    
 };
