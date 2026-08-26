628b83c961dfe73c626f5739fe310bacb1173696
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-26 18:13:18 +0530

    Time: 35 ms (54.31%), Space: 44.3 MB (47.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-     int n = coins.size();
-     vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
-     for(int i = 0; i<n;i++) dp[i][0] = 0;
-       for(int k = 0;k<=amount;k++){
-        if(coins[0] <= k) dp[0][k] = 1+ dp[0][k-coins[0]];
+    int coinChange(vector<int>& coins, int amount){
+      int n = coins.size();
+ 
+        vector<int>prev(amount+1,1e9);
+        prev[0] = 0;
+       for(int k = 1 ;k <= amount;k++){
+             if(coins[0] <= k) prev[k] = 1+prev[k-coins[0]];
        }
-      for(int i = 1;i<n;i++){
-         for(int k = 0 ;k <= amount;k++){
-             int take = 1e9;
-             int nottake = dp[i-1][k];
-             if(coins[i] <= k) take = 1+dp[i][k-coins[i]];
-             dp[i][k] = min(take,nottake);
+
+         for(int i = 1;i<n;i++){
+              vector<int>cur(amount+1,1e9);
+                cur[0] = 0;
+              for(int k = 1 ;k <= amount;k++){
+                     cur[k] = prev[k];
+                      if(coins[i] <= k){
+                       cur[k] = min(cur[k] ,  1+cur[k-coins[i]] ) ;
+                 }
+            }
+            swap(cur,prev);
          }
-      }
-
-      if(dp[n-1][amount] < 1e9) return dp[n-1][amount];
-      return -1;
-
+     
+         int ans = prev[amount];
+      return (ans >= 1e9 ? -1 : ans);
         
     
     }
