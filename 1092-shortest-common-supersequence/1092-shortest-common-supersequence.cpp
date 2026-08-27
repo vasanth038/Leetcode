b4c2fb4c7f672543fab74d9b9de92083e853488e
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-27 14:48:43 +0530

    Time: 25 ms (58.79%), Space: 26.8 MB (43.25%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-         
-         int n = str1.size();
-         int m = str2.size();
 
-         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-         
-         for(int i = 1;i<=n;i++){
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i = 1;i<=n;i++){
+
             for(int j = 1;j<=m;j++){
-                if(str1[i-1] == str2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
-                else dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
-            }
-         }
 
-         string res;
-           int i = n;
-           int j = m;
-            
-            while(i > 0 && j > 0){
-                if(str1[i-1] == str2[j-1]) {
-                    res+=str1[i-1];
-                    i--;
-                    j--;
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }
-                else if( dp[i][j-1] >= dp[i-1][j]){
-                     res+= str2[j-1];
-                     j--;
-                }
-                else{
-                     res+=str1[i-1];
-                     i--;
-                }
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
-            while(i > 0){
-                 res+=str1[i-1];
-                     i--;
+        } 
+
+        string res = "";
+
+        int i = n;
+        int j = m;
+
+        while(i > 0 || j > 0){
+            if( i > 0 && j > 0 && str1[i-1] == str2[j-1] ){
+
+                res.push_back(str1[i-1]);
+                i--;
+                j--;
             }
-            while(j > 0){
-               res+= str2[j-1];
-                     j--; 
+            else if(i > 0 && dp[i][j] == dp[i-1][j]){
+                 res.push_back(str1[i-1]);
+                i--;
+
             }
+            else if(j > 0){
+                res.push_back(str2[j-1]);
+                j--;
+            }
+        }
+
       reverse(res.begin(),res.end());
 
       return res;
-       
+
+        
     }
 };
