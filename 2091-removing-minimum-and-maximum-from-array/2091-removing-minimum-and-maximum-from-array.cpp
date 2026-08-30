53370fdf622647388ef974450ed15a62a1ddb7e3
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-08-30 19:44:52 +0530

    Time: 8 ms (6.69%), Space: 92.2 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -5,15 +5,20 @@ public:
         int n = nums.size();
         if(n <= 2) return n;
 
-         int maxIdx = max_element(nums.begin(),nums.end())-nums.begin();
-          int minIdx = min_element(nums.begin(),nums.end())-nums.begin();
+         int maxIdx = 0;
+          int minIdx =  0 ;
+            
+           for(int i = 1;i<n;i++){
+            if(nums[i] > nums[maxIdx]) maxIdx = i;
+             if(nums[i] < nums[minIdx]) minIdx = i;
+           }
           
              int leftMost = min(maxIdx,minIdx);
              int rightMost = max(maxIdx,minIdx);
              int ans = min(n-leftMost , rightMost+1);
               int x = min( maxIdx+1, n-maxIdx );
                int y = min( minIdx+1, n-minIdx );
-            ans = min(ans, x+y);
+            ans = min( ans, x+y);
           return ans;
     }
 };
