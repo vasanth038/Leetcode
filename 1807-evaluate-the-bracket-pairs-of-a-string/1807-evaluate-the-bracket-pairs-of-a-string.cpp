5bb413c0408d780db307caaa939c9d613a76f051
S Vasantha Kumar <vasanth9902308856@gmail.com>
2026-09-26 15:13:43 +0530

    Time: 71 ms (81.93%), Space: 122.7 MB (66.67%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,37 +2,25 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
 
-        map<string ,string>mpp;
+        unordered_map<string ,string>mpp;
 
-        for(auto st : knowledge){
+        for(auto& st : knowledge){
              mpp[st[0]] = st[1];
         }
 
         string res = "";
-
-       int i = 0;
        int n = s.size();
-       while(i < n ){
+       
+       for(int i = 0;i<n;i++){
+           
+           if(s[i] == '('){
 
-         if(s[i] == '('){
-                i++;
-                 string key = "";
-             while( i < n && s[i] != ')'){
-                key+=s[i];
-                i++;
-             }
-             i++;
-            
-            if(mpp.count(key)){
-                 res+=mpp[key];
-            }
-            else res+='?';
-
-         }
-         else {
-            res+=s[i];
-            i++;
-         }
+             int j = s.find(")" , i+1);
+             auto key = s.substr(i+1 , j-i-1);
+             res+=  mpp.count(key) ? mpp[key] : "?" ;
+              i = j ;
+           }
+           else res+=s[i];
          
        }
 
